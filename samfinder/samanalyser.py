eeccdc834ae7dc4c52babd0f5453d63e935cc16f
spio <spio@tiguriner>
2019-05-07 13:12:19 +0200

    - addition of per-folder statistics

--- samfinder/samanalyser.py
+++ samfinder/samanalyser.py
@@ -8,11 +8,13 @@ yaml.add_multi_constructor('!', lambda loader, suffix, node: None)
 
 types = {}
 runtimes = {}
+foldertypes = {}
 allsams = 0
 
 print("Scanning SAMs...")
 samfolders = glob.glob("extracted-sams/*")
 for samfolder in samfolders:
+	localtypes = {}
 	sams = os.listdir(samfolder)
 	allsams += len(sams)
 	for sam in sams:
@@ -26,6 +28,7 @@ for samfolder in samfolders:
 					else:
 						print("R {} {}".format(r, t))
 					types[t] = types.get(t, 0) + 1
+					localtypes[t] = 1
 
 					if t == "AWS::Serverless::Function":
 						if "Properties" in y["Resources"][r]:
@@ -35,12 +38,16 @@ for samfolder in samfolders:
 				else:
 					print("R {} <untyped>?".format(r))
 
+	for t in localtypes:
+		foldertypes[t] = foldertypes.get(t, 0) + 1
+
 print("Evaluating {} SAMs...".format(allsams))
 #types = collections.OrderedDict(sorted(types.items(), key=lambda x: x[1], reverse=True))
 #print(types)
 for rtype in sorted(types):
 	pct = round(100 * types[rtype] / allsams, 1)
-	print("{:50s} {:3d} ({:4.1f}%)".format(rtype, types[rtype], pct))
+	fpct = round(100 * foldertypes.get(rtype, 0) / len(samfolders), 1)
+	print("{:50s} {:3d} ({:4.1f}%) [folder: {:3d} ({:4.1f}%)]".format(rtype, types[rtype], pct, foldertypes.get(rtype, 0), fpct))
 
 print("Evaluating runtimes...")
 numfunctions = types["AWS::Serverless::Function"]
