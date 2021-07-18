aef585fe5e0ec54e4e03c5a29305f74d622cc383
Daniel Haß <danielhass@users.noreply.github.com>
2021-07-18 15:05:47 +0200

    reworked cli + added timestamp and light option

--- cli.py
+++ cli.py
@@ -1,62 +1,68 @@
 import argparse
 import subprocess
 import os
+import shutil
 from pathlib import Path
+from datetime import datetime
 
-def _acquire_sar():
+timestamp = False
+light = False
+output_dir = 'data'
+
+def _run_cmd_datadir(cmd, output_dir):
     old_wd = os.getcwd()
-    _output_path = 'data/autostats'
-    Path(_output_path).mkdir(parents=True, exist_ok=True)
-    os.chdir(_output_path)
-    subprocess.run(f"python {old_wd}/autocontents.py --custom", shell=True)
+    Path(output_dir).mkdir(parents=True, exist_ok=True)
+    os.chdir(output_dir)
+    subprocess.run(f"python {old_wd}/{cmd}", shell=True)
     os.chdir(old_wd)
+
+def _autocontents():
+    _output_path = f'{output_dir}/autostats'
+    _run_cmd_datadir("autocontents.py --custom", _output_path)
 
 def _insights():
-    old_wd = os.getcwd()
-    os.chdir('data/')
-    subprocess.run(f"python {old_wd}/insights.py", shell=True)
-    os.chdir(old_wd)
+    _run_cmd_datadir("insights.py", output_dir)
 
 def _insights_plot():
-    old_wd = os.getcwd()
-    os.chdir('data/')
-    subprocess.run(f"python {old_wd}/insights-plot.py", shell=True)
-    os.chdir(old_wd)
+    _run_cmd_datadir("insights-plot.py", output_dir)
 
 def _codechecker():
-    old_wd = os.getcwd()
-    os.chdir('data/')
-    subprocess.run(f"python {old_wd}/codechecker.py", shell=True)
-    os.chdir(old_wd)
+    _run_cmd_datadir("codechecker.py", output_dir)
 
 def _samfinder():
-    old_wd = os.getcwd()
-    os.chdir('data/')
-    subprocess.run(f"python {old_wd}/samfinder/samfinder.py --copy", shell=True)
-    os.chdir(old_wd)
+    _run_cmd_datadir("samfinder/samfinder.py --copy", output_dir)
 
 def _github_contents():
-    old_wd = os.getcwd()
-    _output_path = 'data/github-contents'
-    Path(_output_path).mkdir(parents=True, exist_ok=True)
-    os.chdir(_output_path)
-    subprocess.run(f"python {old_wd}/github-contents.py ../autostats/autocontents-*.csv", shell=True)
-    os.chdir(old_wd)
+    _output_path = f'{output_dir}/github-contents'
+    _run_cmd_datadir("github-contents.py ../autostats/autocontents-*.csv", _output_path)
+
+def _remove_large_results():
+    _rm_dirs = ['_codechecker', '_codefolders', '_codestamp']
+    for dir in _rm_dirs:
+        dirpath = Path(f'{output_dir}/{dir}')
+        # check if path exists
+        if dirpath.exists() and dirpath.is_dir():
+            # remove files recursively
+            shutil.rmtree(dirpath)
 
 cli = argparse.ArgumentParser(description='AWS SAR Analysis')
 
 cli.add_argument(
     "task",
-    choices=['acquire-sar', 'insights', 'insights-plot', 'github-contents', 'codechecker', 'samfinder', 'all'],
+    choices=['autocontents', 'insights', 'insights-plot', 'github-contents', 'codechecker', 'samfinder', 'all'],
     default='all',
     nargs='?',
     help='Running without parameters will execute all tasks.'
     )
 
+cli.add_argument('--timestamp', dest='timestamp', action='store_true', default=False, help='non CSV output data will be generated in a timestamped subfolder (e.g. 2021-01-01/)')
+cli.add_argument('--light', dest='light', action='store_true', default=False, help='remove large cache files from data dir after execution')
+
 args = cli.parse_args()
-if args.task == 'acquire-sar':
+
+if args.task == 'autocontents':
     # get metadata from SAR
-    _acquire_sar()
+    _autocontents()
 elif args.task == 'insights':
     # generate insights from SAR data
     _insights()
@@ -72,9 +78,27 @@ elif args.task == 'samfinder':
     _samfinder()
 elif args.task == 'all':
     # generate insights from SAR data
-    _acquire_sar()
+    _autocontents()
     _insights()
     _insights_plot()
     #_github_contents()
     _codechecker()
     _samfinder()
+
+if args.timestamp:
+    timestamp = True
+    timestamp_dir = f"{output_dir}/{datetime.today().strftime('%Y-%m-%d')}"
+    Path(timestamp_dir).mkdir(parents=True, exist_ok=True)
+    _to_move = [
+        'codecheckerfolders.json',
+        'codecheckernamedfolders.json',
+        'codecheckerrepos.json',
+        'plots',
+        '_sams'
+    ]
+    for path in _to_move:
+        shutil.move(f'{output_dir}/{path}', f'{timestamp_dir}/{path}')
+
+if args.light:
+    light = True
+    _remove_large_results()
